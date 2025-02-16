f82400392d3a20ae7231911f8a29e70ea5e4de32
victoria parchevskaya <parchevskayavictoria@gmail.com>
2025-02-17 01:47:45 +0300

    feat: init

---
+++ number-generate-service/src/main/java/edu/spv/numbergenerateservice/NumberGenerateServiceApplication.java
@@ -0,0 +1,12 @@
+package edu.spv.numbergenerateservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NumberGenerateServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NumberGenerateServiceApplication.class, args);
+    }
+}
