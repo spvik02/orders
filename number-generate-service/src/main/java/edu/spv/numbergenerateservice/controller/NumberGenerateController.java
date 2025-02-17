444ef7fb9c64756877c302f250ed12c96720c467
victoria parchevskaya <parchevskayavictoria@gmail.com>
2025-02-17 03:09:23 +0300

    feat: add swagger

--- number-generate-service/src/main/java/edu/spv/numbergenerateservice/controller/NumberGenerateController.java
+++ number-generate-service/src/main/java/edu/spv/numbergenerateservice/controller/NumberGenerateController.java
@@ -1,6 +1,9 @@
 package edu.spv.numbergenerateservice.controller;
 
 import edu.spv.numbergenerateservice.service.NumberGenerateService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -11,10 +14,15 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/numbers")
+@Tag(name = "Number Generation", description = "API для генерации уникальных номеров заказов")
 public class NumberGenerateController {
 
     private final NumberGenerateService numberGenerateService;
 
+    @Operation(summary = "Сгенерировать уникальный номер заказа",
+            description = "Возвращает сгенерированный номер заказа (5 символов) и текущую дату (формат YYYYMMDD), пример 1111120241212")
+    @ApiResponse(responseCode = "200", description = "Успешно сгенерирован номер заказа")
+    @ApiResponse(responseCode = "500", description = "Ошибка на сервере")
     @GetMapping
     public ResponseEntity<String> getNumber() {
         String generatedUniqueNumber = numberGenerateService.generateUniqueNumber();
